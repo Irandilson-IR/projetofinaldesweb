27a99db3943d4ddae1b48a191e36967fccfbea4f
Irandilson2020 <irandilson@alu.ufc.br>
2021-07-11 22:36:10 -0300

    Projeto criado

---
+++ src/main/java/com/iran/projetofinaldesweb/ProjetofinaldeswebApplication.java
@@ -0,0 +1,13 @@
+package com.iran.projetofinaldesweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofinaldeswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofinaldeswebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iran/projetofinaldesweb/ProjetofinaldeswebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iran.projetofinaldesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofinaldeswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
