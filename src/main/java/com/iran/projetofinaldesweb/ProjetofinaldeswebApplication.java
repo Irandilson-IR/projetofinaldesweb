507f31d90f3b087ecacc43ac073f2366dc2b978e
Irandilson2020 <irandilson@alu.ufc.br>
2021-07-12 23:15:39 -0300

    Inserção Produtos e associação N:N (muitos para muitos)

--- src/main/java/com/iran/projetofinaldesweb/ProjetofinaldeswebApplication.java
+++ src/main/java/com/iran/projetofinaldesweb/ProjetofinaldeswebApplication.java
@@ -8,12 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.iran.projetofinaldesweb.domain.Categoria;
+import com.iran.projetofinaldesweb.domain.Produto;
 import com.iran.projetofinaldesweb.repositories.CategoriaRepository;
+import com.iran.projetofinaldesweb.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ProjetofinaldeswebApplication implements CommandLineRunner{
+	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetofinaldeswebApplication.class, args);
 	}
@@ -23,8 +28,21 @@ public class ProjetofinaldeswebApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informátia");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-	
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 	}
 
 }
--- src/main/java/com/iran/projetofinaldesweb/domain/Categoria.java
+++ src/main/java/com/iran/projetofinaldesweb/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.iran.projetofinaldesweb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 
@@ -17,6 +20,10 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
+	
+		
 	public Categoria() {
 		
 	}
@@ -41,6 +48,14 @@ public class Categoria implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override
@@ -68,6 +83,8 @@ public class Categoria implements Serializable {
 				return false;
 		return true;
 	}
+
+	
 	
 	
 
