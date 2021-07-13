eeebd0ffbe049c8be564cdcc3e46fa5d1675f62e
Irandilson2020 <irandilson@alu.ufc.br>
2021-07-12 22:16:27 -0300

    Criação de uma operação de instanciação

--- src/main/java/com/iran/projetofinaldesweb/ProjetofinaldeswebApplication.java
+++ src/main/java/com/iran/projetofinaldesweb/ProjetofinaldeswebApplication.java
@@ -1,13 +1,30 @@
 package com.iran.projetofinaldesweb;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetofinaldeswebApplication {
+import com.iran.projetofinaldesweb.domain.Categoria;
+import com.iran.projetofinaldesweb.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetofinaldeswebApplication implements CommandLineRunner{
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetofinaldeswebApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informátia");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	
 	}
 
 }
