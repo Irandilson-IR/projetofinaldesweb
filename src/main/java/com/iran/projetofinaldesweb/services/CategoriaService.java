02faa93add51e96713effc4832a0ed7fff710f78
Irandilson2020 <irandilson@alu.ufc.br>
2021-07-15 08:50:04 -0300

    Implementação de Recursos de Páginação e Parâmetros Opcionais na Requisição usando o POSTMAN

--- src/main/java/com/iran/projetofinaldesweb/services/CategoriaService.java
+++ src/main/java/com/iran/projetofinaldesweb/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.iran.projetofinaldesweb.domain.Categoria;
@@ -47,7 +50,15 @@ public class CategoriaService { //Classe responsável pela consulta no repositor
 		public List<Categoria> findAll() {
 			return repo.findAll();
 		}
+		
+		//Recursos de Paginação
+		public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+			return repo.findAll(pageRequest);
 		}
+		
+		}
+		
 		
 	
 
