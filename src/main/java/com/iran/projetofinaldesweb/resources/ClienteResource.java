b96cbc400f0d752b428245d09718e14987c9b616
Irandilson2020 <irandilson@alu.ufc.br>
2021-07-15 23:41:26 -0300

    Implementando a personalização do retorno da validação

--- src/main/java/com/iran/projetofinaldesweb/resources/ClienteResource.java
+++ src/main/java/com/iran/projetofinaldesweb/resources/ClienteResource.java
@@ -1,14 +1,23 @@
 package com.iran.projetofinaldesweb.resources;
 
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.iran.projetofinaldesweb.domain.Categoria;
 import com.iran.projetofinaldesweb.domain.Cliente;
+import com.iran.projetofinaldesweb.dto.CategoriaDTO;
+import com.iran.projetofinaldesweb.dto.ClienteNewDTO;
 import com.iran.projetofinaldesweb.services.ClienteService;
 
 @RestController
@@ -22,5 +31,14 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/iran/projetofinaldesweb/services/ClienteService.java
+++ src/main/java/com/iran/projetofinaldesweb/services/ClienteService.java
@@ -10,8 +10,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.iran.projetofinaldesweb.domain.Cidade;
 import com.iran.projetofinaldesweb.domain.Cliente;
+import com.iran.projetofinaldesweb.domain.Endereco;
+import com.iran.projetofinaldesweb.domain.enums.TipoCliente;
 import com.iran.projetofinaldesweb.dto.ClienteDTO;
+import com.iran.projetofinaldesweb.dto.ClienteNewDTO;
+import com.iran.projetofinaldesweb.repositories.CidadeRepository;
 import com.iran.projetofinaldesweb.repositories.ClienteRepository;
 import com.iran.projetofinaldesweb.services.exceptions.DataIntegrityException;
 import com.iran.projetofinaldesweb.services.exceptions.ObjectNotFoundException;
@@ -21,6 +26,8 @@ public class ClienteService { //Classe responsável pela consulta no repository
 	
 	@Autowired
 	private ClienteRepository repo;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -62,6 +69,21 @@ public class ClienteService { //Classe responsável pela consulta no repository
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 		
 	}	
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
