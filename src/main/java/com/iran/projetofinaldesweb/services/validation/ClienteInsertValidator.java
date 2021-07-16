d3c98b9fa66328074418f2c7bfeb5b7dc8de4218
Irandilson2020 <irandilson@alu.ufc.br>
2021-07-16 15:06:45 -0300

    Implmentação da validação customizada de CPF e/ou CNPJ na isersção do cliente

---
+++ src/main/java/com/iran/projetofinaldesweb/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,50 @@
+package com.iran.projetofinaldesweb.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.iran.projetofinaldesweb.domain.Cliente;
+import com.iran.projetofinaldesweb.domain.enums.TipoCliente;
+import com.iran.projetofinaldesweb.dto.ClienteNewDTO;
+import com.iran.projetofinaldesweb.repositories.ClienteRepository;
+import com.iran.projetofinaldesweb.resources.exception.FieldMessage;
+import com.iran.projetofinaldesweb.services.validation.utils.BR;
+
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+				
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
+
