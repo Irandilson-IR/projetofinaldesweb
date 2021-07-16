2631584764b41934aa2c068d9c9ab97f21c27ac5
Irandilson2020 <irandilson@alu.ufc.br>
2021-07-16 16:03:26 -0300

    Validação customizada: email não repetido na atualização de cliente

--- src/main/java/com/iran/projetofinaldesweb/dto/ClienteDTO.java
+++ src/main/java/com/iran/projetofinaldesweb/dto/ClienteDTO.java
@@ -8,9 +8,10 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.iran.projetofinaldesweb.domain.Cliente;
+import com.iran.projetofinaldesweb.services.validation.ClienteUpdate;
 
 
-
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
