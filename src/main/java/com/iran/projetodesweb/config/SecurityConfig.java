3baa5a9e09d11df51bddb84140a3eef3a18990da
Irandilson2020 <irandilson@alu.ufc.br>
2021-07-20 13:06:27 -0300

    Implementando autorização de login

--- src/main/java/com/iran/projetodesweb/config/SecurityConfig.java
+++ src/main/java/com/iran/projetodesweb/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.iran.projetodesweb.security.JWTAuthenticationFilter;
+import com.iran.projetodesweb.security.JWTAuthorizationFilter;
 import com.iran.projetodesweb.security.JWTUtil;
 
 
@@ -68,7 +69,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-		
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
