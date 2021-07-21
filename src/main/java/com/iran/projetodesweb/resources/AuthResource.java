f5d400cafb925ffc326e5d9aaa14ec6a6185a5f8
Irandilson2020 <irandilson@alu.ufc.br>
2021-07-20 21:01:14 -0300

    Expondo o header Authorization

--- src/main/java/com/iran/projetodesweb/resources/AuthResource.java
+++ src/main/java/com/iran/projetodesweb/resources/AuthResource.java
@@ -32,8 +32,10 @@ public class AuthResource {
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
+		
 		response.addHeader("Authorization", "Bearer " + token);
 		response.addHeader("access-control-expose-headers", "Authorization");
+		
 		return ResponseEntity.noContent().build();
 	}
 	
