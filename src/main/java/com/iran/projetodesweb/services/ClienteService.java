3e7e390d10b3f39dfc3e60ab1b427454f8914d6e
Irandilson2020 <irandilson@alu.ufc.br>
2021-07-20 21:14:02 -0300

    Ajustando Endpoit para buscar cliente por email e outras correçoes

--- src/main/java/com/iran/projetodesweb/services/ClienteService.java
+++ src/main/java/com/iran/projetodesweb/services/ClienteService.java
@@ -5,8 +5,6 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -15,8 +13,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.amazonaws.services.licensemanager.model.AuthorizationException;
 import com.iran.projetodesweb.domain.Cidade;
 import com.iran.projetodesweb.domain.Cliente;
 import com.iran.projetodesweb.domain.Endereco;
@@ -27,12 +27,13 @@ import com.iran.projetodesweb.dto.ClienteNewDTO;
 import com.iran.projetodesweb.repositories.ClienteRepository;
 import com.iran.projetodesweb.repositories.EnderecoRepository;
 import com.iran.projetodesweb.security.UserSS;
-import com.iran.projetodesweb.services.exceptions.AuthorizationException;
 import com.iran.projetodesweb.services.exceptions.DataIntegrityException;
 import com.iran.projetodesweb.services.exceptions.ObjectNotFoundException;
 
+
+
 @Service
-public class ClienteService { //Classe responsável pela consulta no repository
+public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repo;
@@ -61,10 +62,11 @@ public class ClienteService { //Classe responsável pela consulta no repository
 		if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
 			throw new AuthorizationException("Acesso negado");
 		}
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
-		}
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
 	
 	@Transactional
 	public Cliente insert(Cliente obj) {
@@ -79,7 +81,7 @@ public class ClienteService { //Classe responsável pela consulta no repository
 		updateData(newObj, obj);
 		return repo.save(newObj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
@@ -89,11 +91,25 @@ public class ClienteService { //Classe responsável pela consulta no repository
 			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
+	
 	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
 	
-	//Recursos de Paginação
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+	
+		Cliente obj = repo.findByEmail(email);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + user.getId() + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+	
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
@@ -101,8 +117,7 @@ public class ClienteService { //Classe responsável pela consulta no repository
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);
-		
-	}	
+	}
 	
 	public Cliente fromDTO(ClienteNewDTO objDto) {
 		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
@@ -119,9 +134,6 @@ public class ClienteService { //Classe responsável pela consulta no repository
 		return cli;
 	}
 	
-	
-	
-	
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
@@ -132,18 +144,16 @@ public class ClienteService { //Classe responsável pela consulta no repository
 		if (user == null) {
 			throw new AuthorizationException("Acesso negado");
 		}
+		
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
+		String fileName = prefix + user.getId() + ".jpg";
+		
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
-	
-	BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
-	jpgImage = imageService.cropSquare(jpgImage);
-	jpgImage = imageService.resize(jpgImage, size);
-	
-	String fileName = prefix + user.getId() + ".jpg";
-	
-	return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 }
-}
-	
 
 
 
